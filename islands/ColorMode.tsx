ff3a0124e43a34e4fdeb35373d7ec0e6f449e6f5
= <=>
2023-05-29 09:44:45 +0200

    Added animation to ColorMode-button

--- islands/ColorMode.tsx
+++ islands/ColorMode.tsx
@@ -1,9 +1,9 @@
+import { useState } from "preact/hooks";
+import { Head } from "$fresh/runtime.ts";
 import IconMoon from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/moon.tsx";
 import IconSun from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/sun.tsx";
 
-import { useState } from "preact/hooks";
-
-export type Mode = "light" | "dark";
+type Mode = "light" | "dark";
 
 interface ColorModeProps {
   initColorMode: Mode;
@@ -11,12 +11,66 @@ interface ColorModeProps {
 
 export default function ColorMode(props: ColorModeProps) {
   const [colorMode, setColorMode] = useState(props.initColorMode);
+
+  const handleClick = () => {
+    setColorMode(colorMode === "dark" ? "light" : "dark");
+  };
+
   return (
-    <button
-      class="rounded-full px-2 py-1 md:border(gray-100 2) focus:outline-none hover:bg-gray-200 w-12 h-12 flex items-center justify-center"
-      onClick={() => setColorMode(colorMode === "dark" ? "light" : "dark")}
-    >
-      {colorMode === "dark" ? <IconMoon /> : <IconSun />}
-    </button>
+    <>
+      <Head>
+        <link rel="stylesheet" href="/styles/_colormode.css" />
+      </Head>
+      <button
+        class={`rounded-full p-2 border-gray-100 border-2 focus:outline-none hover:bg-gray-200 w-12 h-12 flex items-center justify-center transition-colors ${
+          colorMode === "dark" ? "bg-dark" : "bg-light"
+        }`}
+        onClick={handleClick}
+      >
+        <div class="relative w-8 h-8">
+          {colorMode === "dark"
+            ? (
+              <>
+                <IconMoon
+                  class="w-full h-full absolute top-0 left-0 animate-slide-in"
+                  style={{
+                    animationFillMode: "forwards",
+                    animationDuration: "0.3s",
+                    animationTimingFunction: "ease",
+                  }}
+                />
+                <IconSun
+                  class="w-full h-full absolute top-0 left-0 animate-slide-out"
+                  style={{
+                    animationFillMode: "forwards",
+                    animationDuration: "0.3s",
+                    animationTimingFunction: "ease",
+                  }}
+                />
+              </>
+            )
+            : (
+              <>
+                <IconSun
+                  class="w-full h-full absolute top-0 left-0 animate-slide-in"
+                  style={{
+                    animationFillMode: "forwards",
+                    animationDuration: "0.3s",
+                    animationTimingFunction: "ease",
+                  }}
+                />
+                <IconMoon
+                  class="w-full h-full absolute top-0 left-0 animate-slide-out"
+                  style={{
+                    animationFillMode: "forwards",
+                    animationDuration: "0.3s",
+                    animationTimingFunction: "ease",
+                  }}
+                />
+              </>
+            )}
+        </div>
+      </button>
+    </>
   );
 }
