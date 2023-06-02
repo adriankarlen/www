8194a3b605e2b7352c975c9bd4c3511836c9c905
= <=>
2023-06-02 15:08:04 +0200

    Fix smooth scroll

--- components/Scroller.tsx
+++ components/Scroller.tsx
@@ -1,4 +1,3 @@
-import { useRef } from "preact/hooks";
 import IconChevronsDown from "@tabler/icons/chevrons-down.tsx";
 
 interface ScrollerProps {
@@ -6,18 +5,9 @@ interface ScrollerProps {
 }
 
 export default function Scroller(props: ScrollerProps) {
-  const mainRef = useRef<HTMLDivElement>(null);
-
-  const handleScroll = () => {
-    if (mainRef.current) {
-      mainRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
   return (
     <a
       href={props.href}
-      onClick={handleScroll}
       class="hover:scale-110 transition-transform duration-300 animate-pulse"
     >
       <IconChevronsDown />
