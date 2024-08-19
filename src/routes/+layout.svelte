c96ba3b5866c0ed72869763bbfe5d6a3d22fd354
Adrian Karlén <113891532+adriankarlen@users.noreply.github.com>
2024-08-19 07:49:31 +0200

    feat!: rework of page (#2)
    * feat!: move to svelte
    
    * feat: start, about and projects pages
    
    * fix(projects): make entire cards clickable
    
    * fix: add favicon and title

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import "../app.css";
+  import {
+    IconBrandGithub,
+    IconBrandLinkedin,
+    IconFileDownload,
+    IconMail
+  } from "@tabler/icons-svelte";
+  import Button from "../components/Button.svelte";
+</script>
+
+<nav class="flex justify-between p-5 bg-rp-overlay border-b-4 border-rp-text">
+  <div class="flex gap-2">
+    <a href="/">
+      <img
+        class="w-10 h-10 border-2 border-rp-text rounded-full"
+        src="/misc/avatar.png"
+        alt="avatar"
+      />
+    </a>
+  </div>
+  <div class="flex items-center gap-10 text-xl font-medium">
+    <a href="/about-me">About me</a>
+    <a href="/projects">Projects</a>
+  </div>
+  <div class="flex items-center gap-5">
+    <Button link="https://github.com/adriankarlen">
+      <IconBrandGithub class="w-full h-full p-1 text-rp-pine" />
+    </Button>
+    <Button link="mailto:akarlen@pm.me">
+      <IconMail class="w-full h-full p-1 text-rp-gold" />
+    </Button>
+    <Button link="https://www.linkedin.com/in/adrian-karlen">
+      <IconBrandLinkedin class="w-full h-full p-1 text-rp-rose" />
+    </Button>
+    <Button link="https://adriankarlen.dev/adriankarlen-resume.pdf">
+      <IconFileDownload class="w-full h-full p-1 text-rp-love" />
+    </Button>
+  </div>
+</nav>
+<slot />
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Card from "../../components/Card.svelte";
+</script>
+
+<div
+  class="flex flex-col m-auto justify-between items-center w-full 2xl:w-2/3 px-10"
+>
+  <div class="text-4xl lg:text-6xl font-black">
+    <h1>Here are some stuffs I've done.</h1>
+  </div>
+  <div class="my-10 grid grid-cols-1 lg:grid-cols-2 gap-10 font-medium">
+    <a href="https://github.com/adriankarlen/_sushi">
+      <Card imageUrl="/projects/_sushi.webp" alt="sushi startpage">
+        _sushi start page
+      </Card>
+    </a>
+    <a href="https://github.com/adriankarlen/bar.wezterm">
+      <Card imageUrl="/projects/wezterm-bar.png" alt="bar.wezterm">
+        bar.wezterm
+      </Card>
+    </a>
+    <a href="https://github.com/adriankarlen/www">
+      <Card imageUrl="/projects/personal-website.webp" alt="personal website">
+        adriankarlen.dev
+      </Card>
+    </a>
+    <a href="https://github.com/adriankarlen/rose-pine.zebar" target="_blank">
+      <Card imageUrl="/projects/zebar.png" alt="rose-pine zebar">
+        rosé pine zebar
+      </Card>
+    </a>
+  </div>
+</div>
