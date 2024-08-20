07a63573673d95da58926ebc76e2ff0b4ba917c7
adriankarlen <adrian.karlen@spp.se>
2024-08-20 07:17:01 +0200

    fix: stagger animation on elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <div
-  class="flex flex-col m-auto justify-center w-full 2xl:w-2/3 px-10 text-4xl md:text-6xl lg:text-8xl font-black animate-enter"
+  class="flex flex-col m-auto justify-center w-full 2xl:w-2/3 px-10 text-4xl md:text-6xl lg:text-8xl font-black"
 >
-  <h1>
+  <h1 class=" animate-enter">
     Hi, my name is <span class="text-rp-love">Adrian</span>.
   </h1>
-  <h1>
+  <h1 class=" animate-enter" style="--stagger: 1;">
     I'm a <span class="text-rp-rose">full stack developer</span> from
     <span class="text-rp-pine">Stockholm</span>.
   </h1>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,28 +3,43 @@
 </script>
 
 <div
-  class="flex flex-col m-auto justify-between items-center w-full 2xl:w-2/3 px-10 animate-enter"
+  class="flex flex-col m-auto justify-between items-center w-full 2xl:w-2/3 px-10"
 >
-  <div class="text-4xl lg:text-6xl font-black">
+  <div class="text-4xl lg:text-6xl font-black animate-enter">
     <h1>Here are some stuffs I've done.</h1>
   </div>
   <div class="my-10 grid grid-cols-1 lg:grid-cols-2 gap-10 font-medium">
-    <a href="https://github.com/adriankarlen/_sushi">
-      <Card imageUrl="/projects/_sushi.webp" alt="sushi startpage">
-        _sushi
-      </Card>
+    <a
+      href="https://github.com/adriankarlen/_sushi"
+      class=" animate-enter"
+      style="--stagger: 1;"
+    >
+      <Card imageUrl="/projects/_sushi.webp" alt="sushi startpage">_sushi</Card>
     </a>
-    <a href="https://github.com/adriankarlen/bar.wezterm">
+    <a
+      href="https://github.com/adriankarlen/bar.wezterm"
+      class=" animate-enter"
+      style="--stagger: 1;"
+    >
       <Card imageUrl="/projects/wezterm-bar.png" alt="bar.wezterm">
         bar.wezterm
       </Card>
     </a>
-    <a href="https://github.com/adriankarlen/www">
+    <a
+      href="https://github.com/adriankarlen/www"
+      class=" animate-enter"
+      style="--stagger: 2;"
+    >
       <Card imageUrl="/projects/this-site.png" alt="personal website">
         adriankarlen.dev
       </Card>
     </a>
-    <a href="https://github.com/adriankarlen/rose-pine.zebar" target="_blank">
+    <a
+      href="https://github.com/adriankarlen/rose-pine.zebar"
+      target="_blank"
+      class=" animate-enter"
+      style="--stagger: 2;"
+    >
       <Card imageUrl="/projects/zebar.png" alt="rose-pine zebar">
         rosé pine zebar
       </Card>
