c02df4fce067e07eb4ce7d2569f897179af8d8f4
Adrian Karlen <adrian.karlen@spp.se>
2026-01-31 10:22:22 +0100

    fix: various layout issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,22 +16,22 @@
   }
 </script>
 
-<div class="min-h-svh">
+<div class="flex flex-col min-h-svh">
   <header class="relative">
     <nav
-      class="flex justify-between items-center p-5 bg-rp-overlay border-b-4 border-rp-rose"
+      class="flex justify-between items-center p-5 border-b-4 bg-rp-overlay border-rp-rose"
     >
       <div class="flex gap-2">
         <a href="/">
           <img
-            class="w-10 h-10 border-2 border-rp-highlight-med dark:border-rp-highlight-med rounded-full"
+            class="w-10 h-10 rounded-full border-2 border-rp-highlight-med dark:border-rp-highlight-med"
             src="/misc/avatar.png"
             alt="avatar"
           />
         </a>
       </div>
       <div
-        class="flex items-center gap-4 md:gap-10 text-xl font-medium text-rp-subtle"
+        class="flex gap-4 items-center text-xl font-medium md:gap-10 text-rp-subtle"
       >
         <a href="/about-me">About me</a>
         <a href="/projects">Projects</a>
@@ -39,32 +39,32 @@
       <Menu>
         <Button link="https://github.com/adriankarlen">
           <IconBrandGithub
-            class="w-full h-full z-10 p-1 text-rp-pine md:bg-blend-20 md:hover:bg-blend-20 hover:bg-blend-25"
+            class="z-10 p-1 w-full h-full text-rp-pine md:bg-blend-20 md:hover:bg-blend-20 hover:bg-blend-25"
           />
         </Button>
         <Button link="mailto:akarlen@pm.me">
           <IconMail
-            class="w-full h-full p-1 text-rp-gold md:bg-blend-20 md:hover:bg-blend-20 hover:bg-blend-25"
+            class="p-1 w-full h-full text-rp-gold md:bg-blend-20 md:hover:bg-blend-20 hover:bg-blend-25"
           />
         </Button>
         <Button link="https://www.linkedin.com/in/adrian-karlen">
           <IconBrandLinkedin
-            class="w-full h-full p-1 text-rp-rose md:bg-blend-20 md:hover:bg-blend-20 hover:bg-blend-25"
+            class="p-1 w-full h-full text-rp-rose md:bg-blend-20 md:hover:bg-blend-20 hover:bg-blend-25"
           />
         </Button>
         <Button link="https://adriankarlen.dev/adriankarlen-resume.pdf">
           <IconFileDownload
-            class="w-full h-full p-1 text-rp-love md:bg-blend-20 md:hover:bg-blend-20 hover:bg-blend-25"
+            class="p-1 w-full h-full text-rp-love md:bg-blend-20 md:hover:bg-blend-20 hover:bg-blend-25"
           />
         </Button>
       </Menu>
     </nav>
   </header>
-  <main class="grow min-h-full flex flex-col items-center justify-center">
+  <main class="flex flex-col justify-center items-center flex-1 py-10">
     {@render children()}
   </main>
 </div>
-<footer class="bg-rp-overlay p-4 text-center">
+<footer class="p-4 text-center bg-rp-overlay">
   <p>
     Made with love by
     <a
@@ -76,5 +76,7 @@
       adriankarlen
     </a>
   </p>
-  <Button onclick={scrollToTop} class="md:w-max px-4 mt-4">Go to Top</Button>
+  <Button onclick={scrollToTop} class="px-4 mt-4 w-max md:w-max"
+    >Go to Top</Button
+  >
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <div
-  class="flex flex-col m-auto justify-center w-full 2xl:w-2/3 px-10 text-4xl md:text-6xl lg:text-8xl font-black"
+  class="flex flex-col justify-center px-10 m-auto w-full text-5xl font-black md:text-7xl lg:text-8xl 2xl:w-2/3"
 >
-  <h1 class=" animate-enter">
+  <h1 class="animate-enter">
     Hi, my name is <span class="text-rp-love">Adrian</span>.
   </h1>
-  <h1 class=" animate-enter" style="--stagger: 1;">
+  <h1 class="animate-enter" style="--stagger: 1;">
     I'm a <span class="text-rp-rose">full stack developer</span> from
     <span class="text-rp-pine">Stockholm</span>.
   </h1>
